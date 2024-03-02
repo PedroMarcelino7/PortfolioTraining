b9a5b66ff163f04c1986f9fb0e00ce1ad4b47b05
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-02 01:06:39 -0300

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.scss'
+import About from './components/About/About'
 import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
       <Header />
+
+      <About />
     </>
   )
 }
